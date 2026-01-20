c8e19b4a85f1c272ae62dd1dbe11ddfa106fb462
Galli Mattia <mattia.galli9@studio.unibo.it>
2026-01-20 16:07:18 +0100

    added the level implementation

--- src/main/java/snakerunner/model/impl/LevelImpl.java
+++ src/main/java/snakerunner/model/impl/LevelImpl.java
@@ -1,12 +1,16 @@
 package snakerunner.model.impl;
 
+<<<<<<< HEAD
 import java.util.Set;
 import snakerunner.commons.Point2D;
 import snakerunner.model.Grid;
+=======
+>>>>>>> ab5c8e9 (added the level implementation)
 import snakerunner.model.Level;
 
 public class LevelImpl implements Level{
     // GRANDEZZA LIVELLO
+<<<<<<< HEAD
     private final Grid grid;
     private final Set<Point2D<Integer, Integer>> obstacles;
 
@@ -28,5 +32,17 @@ public class LevelImpl implements Level{
     @Override
     public boolean IsBlocked(Point2D<Integer, Integer> position) {
         return !grid.isInsideGrid(position) || obstacles.contains(position);
+=======
+    // private final int width;
+    // private final int height;
+    private final int baseSpeed;
+    private final int ItemCount;
+    private final int levelDuration;
+
+    public LevelImpl(int baseSpeed, int itemCount, int levelDuration) {
+        this.baseSpeed = baseSpeed;
+        this.ItemCount = itemCount;
+        this.levelDuration = levelDuration;
+>>>>>>> ab5c8e9 (added the level implementation)
     }
 }
