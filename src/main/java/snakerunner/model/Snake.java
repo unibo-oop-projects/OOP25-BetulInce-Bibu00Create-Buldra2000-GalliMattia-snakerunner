80c4848bbdd6146b1e79f2714b4a66a50d1fad83
Galli Mattia <mattia.galli9@studio.unibo.it>
2026-02-06 20:09:26 +0100

    added snake interface

--- src/main/java/snakerunner/model/Snake.java
+++ src/main/java/snakerunner/model/Snake.java
@@ -1,12 +1,18 @@
 package snakerunner.model;
 
+import java.util.List;
+
 import snakerunner.commons.Point2D;
 
-public class Snake {
+public interface  Snake {
+    public void move();
 
-    public Point2D<Integer, Integer> getHead() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getHead'");
-    }
-    
+    public void setDirection(Direction direction);
+
+    public Point2D<Integer, Integer> getHead();
+
+    public List<Point2D<Integer, Integer>> getBody();
+
+    public boolean isCollidingWithItself();
+        
 }
